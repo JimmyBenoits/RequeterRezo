4fafcb89faca698fd5753ba8eba52aa7e5a2f48b
Jimmy Benoits <jimmy.benoits@lirmm.fr>
2019-01-08 15:41:12 +0100

    Oublie de l'en-tête de licence pour le fichier Exemple.java

--- src/test/Exemple.java
+++ src/test/Exemple.java
@@ -11,6 +11,25 @@ import requeterrezo.RequeterRezoDump;
 import requeterrezo.Resultat;
 import requeterrezo.Voisin;
 
+/*
+RequeterRezo
+Copyright (C) 2019  Jimmy Benoits
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
 /**
  * RequeterRezo est une API Java permettant de manipuler les données du réseau lexico-sémantique rezoJDM.
  * Pour en savoir plus, rendez-vous à l'adresse suivante : http://www.jeuxdemots.org/jdm-accueil.php
